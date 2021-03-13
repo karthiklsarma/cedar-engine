f57a763c8f6110e0485bc8db125833b3eefeeb64
karthik sarma <karthiklsarma@gmail.com>
2021-03-13 13:06:01 -0800

    added server

--- server/entry.go
+++ server/entry.go
@@ -1,10 +1,30 @@
 package server
 
 import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
 	"github.com/karthiklsarma/cedar-engine/m/logging"
 )
 
 func InitiateServerEntry() {
 	logging.SetInfoLogLevel()
 	logging.Info("Initializing server entry")
+	router := gin.Default()
+	setupRouting(router)
+	router.Run(":8080")
+}
+
+func setupRouting(router *gin.Engine) {
+	router.GET("/status", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "server has started",
+		})
+	})
+
+	router.POST("/login/:user", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("Hello %s", c.Param("user")),
+		})
+	})
 }
