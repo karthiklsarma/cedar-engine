3bc2404ef3f5a572ddf33bfa004dff6c6f32a19c
karthik sarma <karthiklsarma@gmail.com>
2021-03-30 01:29:34 -0700

    added graphql query and updates

--- server/paths.go
+++ server/paths.go
@@ -5,18 +5,74 @@ import (
 	"github.com/karthiklsarma/cedar-engine/m/logging"
 )
 
+var UsersList []User
+
+type User struct {
+	Id       int    `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func StartGraphQlServer() graphql.Schema {
-	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
+	queryFields := graphql.Fields{
+		"users": &graphql.Field{
+			Type:        graphql.NewList(UserType),
+			Description: "List of users",
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "cedar", nil
+				return UsersList, nil
 			},
 		},
 	}
 
-	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	mutationFields := graphql.Fields{
+		"users": &graphql.Field{
+			Type:        UserType,
+			Description: "Add New User",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+				"username": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+				"password": &graphql.ArgumentConfig{
+					Type: graphql.String,
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+
+				id, _ := p.Args["id"].(int)
+				username, _ := p.Args["username"].(string)
+				password, _ := p.Args["password"].(string)
+
+				newUser := User{
+					Id:       id,
+					Username: username,
+					Password: password,
+				}
+
+				UsersList = append(UsersList, newUser)
+				return newUser, nil
+			},
+		},
+	}
+
+	rootQuery := graphql.ObjectConfig{
+		Name:        "Query",
+		Description: "Root Query",
+		Fields:      queryFields,
+	}
+
+	rootMutation := graphql.ObjectConfig{
+		Name:        "Mutation",
+		Description: "Root Mutation",
+		Fields:      mutationFields,
+	}
+
+	schemaConfig := graphql.SchemaConfig{
+		Query:    graphql.NewObject(rootQuery),
+		Mutation: graphql.NewObject(rootMutation),
+	}
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		logging.Fatal("Invalid graphQl schema")
@@ -24,3 +80,18 @@ func StartGraphQlServer() graphql.Schema {
 
 	return schema
 }
+
+var UserType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "User",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.Int,
+		},
+		"username": &graphql.Field{
+			Type: graphql.String,
+		},
+		"password": &graphql.Field{
+			Type: graphql.String,
+		},
+	},
+})
